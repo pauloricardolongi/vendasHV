a2e5c0019440ac85ee89cca906c4440d0c5b0dc4
pauloricardolongi <pauloricardolongi@gmail.com>
2022-06-12 02:00:15 -0300

    classe ContextListener hibernete

---
+++ src/br/com/Vendas/util/ContextListener.java
@@ -0,0 +1,21 @@
+package br.com.Vendas.util;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ContextListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		
+		
+	}
+
+	
+}
